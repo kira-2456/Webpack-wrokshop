63e28e0aac62732edda2ac432e36afe7a8debefd
Avi Gupta <avi.gupta@sprinklr.com>
2023-11-06 22:05:22 +0530

    temp

--- src/App/index.js
+++ src/App/index.js
@@ -4,31 +4,31 @@ import { BrowserRouter, Switch, NavLink as Link, Route } from 'react-router-dom'
 
 const LoadingComponent = () => <h3>please wait...</h3>;
 
-const AsyncContactComponent = Loadable({
+// const AsyncContactComponent = Loadable({
+//   loading: () => <LoadingComponent />,
+//   loader: () => {
+//     import('../components/ContactComponent').then(() => {
+//       import('../components/ProfileComponent').then(() => {
+//         import('../components/AboutComponent').then(() => import('../components/HomeComponent'))
+//       })
+//     })
+//   },
+// });
+//
+const AsyncProfileComponent = Loadable({
   loading: () => <LoadingComponent />,
-  loader: () => {
-    import('../components/ContactComponent').then(() => {
-      import('../components/ProfileComponent').then(() => {
-        import('../components/AboutComponent').then(() => import('../components/HomeComponent'))
-      })
-    })
-  },
+  loader: () => import('../components/ProfileComponent'),
 });
-//
-// const AsyncProfileComponent = Loadable({
-//   loading: () => <LoadingComponent />,
-//   loader: () => import('../components/ProfileComponent'),
-// });
-//
-// const AsyncAboutComponent = Loadable({
-//   loading: () => <LoadingComponent />,
-//   loader: () => import('../components/AboutComponent'),
-// });
-//
-// const AsyncHomeComponent = Loadable({
-//   loading: () => <LoadingComponent />,
-//   loader: () => import('../components/HomeComponent'),
-// });
+
+const AsyncAboutComponent = Loadable({
+  loading: () => <LoadingComponent />,
+  loader: () => import('../components/AboutComponent'),
+});
+
+const AsyncHomeComponent = Loadable({
+  loading: () => <LoadingComponent />,
+  loader: () => import('../components/HomeComponent'),
+});
 
 
 // create sample App component
@@ -44,15 +44,14 @@ class App extends React.Component {
           <div className="menu">
             <Link exact to="/" activeClassName="active">Home</Link>
             <Link exact to="/about" activeClassName="active">About</Link>
-            <Link exact to="/contact" activeClassName="active">Contact</Link>
             <Link exact to="/profile" activeClassName="active">Profile</Link>
           </div>
 
           <Switch>
-            <Route exact path="/" component={ AsyncContactComponent } />
-            <Route exact path="/about" component={ AsyncContactComponent } />
-            <Route exact path="/profile" component={ AsyncContactComponent } />
-            <Route exact path="/contact" component={ AsyncContactComponent } />
+            <Route exact path="/" component={ AsyncHomeComponent } />
+            <Route exact path="/about" component={ AsyncAboutComponent } />
+            <Route exact path="/profile" component={ AsyncProfileComponent } />
+
           </Switch>
         </div>
       </BrowserRouter>
